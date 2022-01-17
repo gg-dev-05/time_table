729fa7afe4f19f5b0b06577c9e4dd5bdc5cb4d3b
gg-dev-05 <garvitgalgat@gmail.com>
2022-01-17 05:45:22 +0530

    link fix

--- src/Links.js
+++ src/Links.js
@@ -20,7 +20,7 @@ function Links() {
 					<a href="https://drive.google.com/drive/folders/1MrL8zvH89NxZGT04CeGoMzYnS5sjTlNbTOZwI_ifis_Lfm3RW4afVeWkEmE1D8ZWGWr-lzph?usp=sharing"
 						target="_blank" rel="noreferrer">CS 304/354</a>
 					<a href="https://www.iiti.ac.in/people/~ayanm/Courses/Spring_2022/CS306/" target="_blank" rel="noreferrer">CS 306</a>
-					<a href="https://classNameroom.google.com/u/0/c/NDUwMjYxMjM0NjAz" target="_blank" rel="noreferrer">CS 308/358</a>
+					<a href="https://classroom.google.com/u/0/c/NDUwMjYxMjM0NjAz" target="_blank" rel="noreferrer">CS 308/358</a>
 					<a href="/" target="_blank" rel="noreferrer">ES 302</a>
 					<a href="https://chandu8542.github.io/NLP2022/" target="_blank" rel="noreferrer" className="a">CS 425</a>
 				</nav>
